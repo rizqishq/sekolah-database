63157646fae2588a3da5d40216e72f1d1b36d992
rizqishq <210781681+rizqishq@users.noreply.github.com>
2025-10-06 17:01:52 +0700

    melakukan operasi insert data

--- sekolah.sql
+++ sekolah.sql
@@ -13,3 +13,23 @@ CREATE TABLE nilai (
     nilai INT,
     FOREIGN KEY (siswa_id) REFERENCES Siswa(id)
 );
+
+-- Insert Data
+INSERT INTO siswa (nama, umur, jurusan)
+VALUES
+('Andi', 16, 'IPA'),
+('Budi', 17, 'IPS'),
+('Citra', 16, 'IPA'),
+('Dewi', 17, 'Bahasa'),
+('Eko', 16, 'IPS');
+
+INSERT INTO nilai (siswa_id, mata_pelajaran, nilai)
+VALUES
+(1, 'Matematika', 85),
+(1, 'Bahasa Inggris', 90),
+(2, 'Ekonomi', 80),
+(3, 'Fisika', 88),
+(3, 'Kimia', 92),
+(4, 'Bahasa Indonesia', 89),
+(5, 'Sosiologi', 84),
+(5, 'Ekonomi', 76);
